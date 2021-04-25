8bc3e02669dd565668d7bddf1c7795fbd837d88f
Kelsey Siman <ksiman14@gmail.com>
2021-04-25 18:50:53 -0400

    feat: guest can delete items from cart & local storage

--- client/components/CartItem.js
+++ client/components/CartItem.js
@@ -9,15 +9,25 @@ class CartItem extends Component {
       quantity: 1,
     };
     this.handleDelete = this.handleDelete.bind(this);
+    this.handleGuestDelete = this.handleGuestDelete.bind(this);
     this.handleChange = this.handleChange.bind(this);
     this.handleGuestChange = this.handleGuestChange.bind(this);
   }
 
   componentDidMount() {
-    if (this.props.isLoggedIn)
+    if (this.props.isLoggedIn) {
       this.setState({
         quantity: this.props.galaxy.orderItems.quantity,
       });
+    } else {
+      const cartItems = JSON.parse(window.localStorage.getItem('orderItems'));
+      const currentItem = cartItems.find(
+        (item) => item.id === this.props.galaxy.id
+      );
+      this.setState({
+        quantity: Number(currentItem.quantity),
+      });
+    }
   }
 
   handleChange(evt) {
@@ -46,40 +56,58 @@ class CartItem extends Component {
   }
 
   handleDelete() {
-    this.props.destroyItem(
-      this.props.cart,
-      this.props.userId,
-      this.props.galaxy.id
+    if (this.props.isLoggedIn) {
+      this.props.destroyItem(
+        this.props.cart,
+        this.props.userId,
+        this.props.galaxy.id
+      );
+    } else {
+      this.handleGuestDelete();
+    }
+  }
+
+  handleGuestDelete() {
+    const cartItems = JSON.parse(window.localStorage.getItem('orderItems'));
+    const updatedItems = cartItems.filter(
+      (item) => item.id !== this.props.galaxy.id
     );
+    window.localStorage.setItem('orderItems', JSON.stringify(updatedItems));
+    console.log(window.localStorage);
+    this.setState({ quantity: 0 });
   }
 
   render() {
     const { name, imageUrl, price } = this.props.galaxy;
     return (
       <div>
-        <div className="cart_item">
-          <img src={imageUrl} />
-          <div>
-            <p className="cart_name">{name}</p>
-            <p>${price / 100}</p>
-            <label htmlFor="quantity">Quantity:</label>
-            <input
-              type="number"
-              min={1}
-              max={100}
-              name="quantity"
-              value={this.state.quantity}
-              onChange={this.handleChange}
-            />
+        {this.state.quantity > 0 ? (
+          <div className="cart_item">
+            <img src={imageUrl} />
+            <div>
+              <p className="cart_name">{name}</p>
+              <p>${price / 100}</p>
+              <label htmlFor="quantity">Quantity:</label>
+              <input
+                type="number"
+                min={1}
+                max={100}
+                name="quantity"
+                value={this.state.quantity}
+                onChange={this.handleChange}
+              />
+            </div>
+            <button
+              className="delete"
+              id="ORDER_ITEM_ID"
+              onClick={this.handleDelete}
+            >
+              X
+            </button>
           </div>
-          <button
-            className="delete"
-            id="ORDER_ITEM_ID"
-            onClick={this.handleDelete}
-          >
-            X
-          </button>
-        </div>
+        ) : (
+          ''
+        )}
       </div>
     );
   }
