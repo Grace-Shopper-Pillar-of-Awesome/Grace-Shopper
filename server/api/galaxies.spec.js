034980a773ca3b2ca36ea7803df6a4ef9927b3be
Kelsey Siman <ksiman14@gmail.com>
2021-04-27 16:13:13 -0400

    test: /api/galaxies get route

--- server/api/galaxies.spec.js
+++ server/api/galaxies.spec.js
@@ -1,5 +1,20 @@
-const seed = require('../../../script/seed');
+const seed = require('../../script/seed');
 const { expect } = require('chai');
 const app = require('../app');
 const request = require('supertest');
 const { db, Galaxy } = require('../db');
+
+describe('Galaxy routes', () => {
+  beforeEach(async () => {
+    await seed();
+  });
+
+  describe('/api/galaxies', () => {
+    it('GET /api/galaxies', async () => {
+      const res = await request(app).get('/api/galaxies').expect(200);
+
+      expect(res.body).to.be.an('array');
+      expect(res.body.length).to.equal(40);
+    });
+  });
+});
